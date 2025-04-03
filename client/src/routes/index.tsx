21324f5ba2affe54e3573a40690b5978848fdd0b
Tomáš Lajda <tomas.lajda0@gmail.com>
2025-04-03 11:37:37 +0300

    added prettier for tailwind classes

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,13 +1,13 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
   return (
     <div>
-      <h3 className=''>Welcome Home!</h3>
+      <h3>Welcome Home!</h3>
     </div>
   );
 }
