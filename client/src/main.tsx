1c8e6bbb343196570b9d3ea92b0d48568a0af44e
Tomáš Lajda <tomas.lajda0@gmail.com>
2025-04-09 00:12:13 +0300

    tested themes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,30 +1,29 @@
-import { RouterProvider, createRouter } from '@tanstack/react-router';
-import { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 
-// Import the generated route tree
-import { ThemeProvider } from './components/theme-provider';
-import { routeTree } from './routeTree.gen';
+import { ThemeProvider } from "./components/theme-provider";
+import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
 
 // Register the router instance for type safety
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
   }
 }
 
 // Render the app
-const rootElement = document.getElementById('root')!;
+const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
         <RouterProvider router={router} />
       </ThemeProvider>
-    </StrictMode>
+    </StrictMode>,
   );
 }
