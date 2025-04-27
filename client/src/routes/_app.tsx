6cfdd0a1701258b0ef339ca9c6e21444cb919775
Tomáš Lajda <tomas.lajda0@gmail.com>
2025-04-27 17:01:33 +0300

    fixed height and scroll bar

--- client/src/routes/_app.tsx
+++ client/src/routes/_app.tsx
@@ -3,9 +3,9 @@ import { AppBar } from "../components/app-bar/app-bar";
 
 export const Route = createFileRoute("/_app")({
   component: () => (
-    <>
+    <div className="max-h-screen overflow-x-auto overflow-y-hidden">
       <AppBar />
       <Outlet />
-    </>
+    </div>
   ),
 });
